facf210e96409a3e895414423d1048d7b98931de
96306351272 <renatolima@setic.ro.gov.br>
2022-07-22 09:52:57 -0400

    Ajustes

--- CursosAlura/-robotron-2000-projeto_inicial/-robotron-2000-projeto_inicial/js/main.js
+++ CursosAlura/-robotron-2000-projeto_inicial/-robotron-2000-projeto_inicial/js/main.js
@@ -1,12 +1,19 @@
-const robotron = document.querySelector("#robotron")
+const subtrair = document.querySelector("#subtrair")
+const somar = document.querySelector("#somar")
+const braco = document.querySelector("#braco")
 
-robotron.addEventListener("click", (evento) => {
-    console.log(evento)
+const controle = document.querySelector(".controle-ajuste")
+
+controle.forEach((elemento) => { 
+    elemento.addEventListener("click", () => {
+        console.log(evento.target)
+    })
 })
 
-function dizOi(nome) {
-    console.log('Oi ' + nome)
-    console.log('Bem-vindo ao Robotron 2000')
+function manipulaDados(operacao) {
+    if(operacao === "-") {
+        braco.value = parseInt(braco.value) - 1
+    } else {
+        braco.value = parseInt(braco.value) + 1
+    }
 }
-
-dizOi("Pedro") 
